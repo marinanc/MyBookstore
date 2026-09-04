3051fceefc4d48053e80ed9f74d7951ecdbb9378
Marina Carrizo <marinancarrizo@gmail.com>
2026-09-04 00:53:14 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Spring Initializr
+
+-Project: Maven
+-Language: Java
+-Spring Boot: 4.1.1
+-Group: com.personalprojects
+-Artifact: MyBookstore
+-Packaging: Jar
+-Configuration: Properties
+-Java: 25
+-Dependencies:
+	Spring Web
+	Spring Data JPA
+	MySQL Driver
+	Spring Boot Dev Tools
+	Lombok
---
+++ src/main/java/com/personalprojects/MyBookstore/MyBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.personalprojects.MyBookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookstoreApplication.class, args);
+	}
+
+}
