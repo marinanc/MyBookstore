4ed94330678b563567e308490dba864130d01c42
Marina Carrizo <marinancarrizo@gmail.com>
2026-09-08 20:38:45 -0300

    Add Book model

---
+++ src/main/java/com/personalprojects/MyBookstore/model/Book.java
@@ -0,0 +1,30 @@
+package com.personalprojects.MyBookstore.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Book {
+    private Long id;
+    private String title;
+    private String isbnEbook;
+    private String isbnPrint;
+    private Integer publishedYear;
+    private Integer pages;
+    private String synopsis;
+    private String linkDownload;
+    private String version;
+    private List<Author> authors;
+    private List<Genero> genres;
+    private Publisher publisher;
+    private Serie serie;
+    private SourceWebsite sourceWebsite;
+}
