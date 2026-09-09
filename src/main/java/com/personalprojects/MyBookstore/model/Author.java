7f0ca7cb1bedec80122d008949c85b69bdc92fcc
Marina Carrizo <marinancarrizo@gmail.com>
2026-09-08 21:30:13 -0300

    Update Author model to map DB

--- src/main/java/com/personalprojects/MyBookstore/model/Author.java
+++ src/main/java/com/personalprojects/MyBookstore/model/Author.java
@@ -1,6 +1,6 @@
 package com.personalprojects.MyBookstore.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +11,16 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(columnDefinition = "TEXT")
     private String bio;
+
+    @ManyToOne
     private Country country;
 }
