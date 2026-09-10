18dea0384f6f6d260bf78126811d6f0c876de86a
Marina Carrizo <marinancarrizo@gmail.com>
2026-09-10 17:30:20 -0300

    Add book dto

--- src/main/java/com/personalprojects/MyBookstore/dto/BookDTO.java
+++ src/main/java/com/personalprojects/MyBookstore/dto/BookDTO.java
@@ -1,4 +1,26 @@
 package com.personalprojects.MyBookstore.dto;
 
+import lombok.*;
+
+import java.util.Set;
+
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookDTO {
+    private Long id;
+    private String title;
+    private String isbnEbook;
+    private String isbnPrint;
+    private Integer publishedYear;
+    private Integer pages;
+    private String synopsis;
+    private String linkDownload;
+    private String version;
+    private Set<AuthorDTO> authors;
+    private Set<GenreDTO> genres;
+    private Long idPublisher;
+    private Long idSerie;
+    private Long idSource;
 }
