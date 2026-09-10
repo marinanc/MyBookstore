6f0d59b41b2ebf68329bc333582465e0b0215f16
Marina Carrizo <marinancarrizo@gmail.com>
2026-09-10 17:25:05 -0300

    Add author dto

--- src/main/java/com/personalprojects/MyBookstore/dto/AuthorDTO.java
+++ src/main/java/com/personalprojects/MyBookstore/dto/AuthorDTO.java
@@ -1,4 +1,17 @@
 package com.personalprojects.MyBookstore.dto;
 
+import lombok.*;
+
+import java.util.Set;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthorDTO {
+    private Long id;
+    private String name;
+    private String bio;
+    private Long idCountry;
+    private Set<BookDTO> books;
 }
